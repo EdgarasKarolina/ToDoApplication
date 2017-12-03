fe66b9c7b5270a6a6db6bc81966ff284bd203095
edgaras <edgarasvilija@gmail.com>
2017-12-03 13:42:55 +0100

    added mocha again

---
+++ test/unittesting.js
@@ -0,0 +1,13 @@
+var expect  = require('chai').expect;
+require( 'mocha-directory' )();
+
+describe("printName()", function(){
+    it("some crap, dont know what im doing", function(){
+        var result = 5;
+        expect(result).to.equal(5);
+    });
+    it("this is also shit", function(){
+        var result = 10;
+        expect(result).to.equal(10);
+    });
+});
